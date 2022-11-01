ac581469f9859563c5dbee99625d9b09bc852042
Enoch Bayode <bayodeenoch@gmail.com>
2022-11-02 05:10:25 +1300

    auth

---
+++ models/users.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema({
+    firstName: {
+        type: String,
+        required: true,
+    },
+    lastName: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    // confirmPassword: {
+    //     type: String,
+    //     required: true,
+    // },
+    
+
+},{ timestamps: true });
+
+const User = mongoose.model("users", userSchema);
+
+module.exports = { User };
---
+++ router.js
@@ -0,0 +1,12 @@
+const { mainrouter } = require('./routes/main');
+const { authrouter } = require('./routes/auth');
+
+
+module.exports = (app) => {
+  app.use(mainrouter);
+
+  app.use('/user', authrouter);
+
+  
+
+};
