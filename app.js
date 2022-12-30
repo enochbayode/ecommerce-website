1a636d621f3e3f3da62799cadc7200ac740f08f9
Enoch Bayode <bayodeenoch@gmail.com>
2022-12-30 09:43:46 -1100

    fix

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 require("dotenv").config();
 const express = require("express");
 
-
 const app = express();
 const PORT = process.env.PORT || 8800;
 const dbURI = process.env.MONGO_URI;
--- models/cart.js
+++ models/cart.js
@@ -21,7 +21,7 @@ const cartSchema = new mongoose.Schema({
         quantity: {
             type: Number,
             required: true,
-            min: 1,
+               min: 1,
             default: 1
         },
 
